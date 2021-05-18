7ad1e38db7ab7114b5cd330e2328de513fbdb29e
luisdev20 <lescobedo.im@gmail.com>
2021-05-18 04:40:17 +0000

    More errors

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        quotient = a / b
+    except (ZeroDivisionError):
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+    return (quotient)
