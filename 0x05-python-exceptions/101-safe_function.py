31ceba5bfa23dd44978f4b837937a5596969b5ed
luisdev20 <lescobedo.im@gmail.com>
2021-05-18 04:57:17 +0000

    blank line

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
-    """Executes a function safely"""
+    """ Executes a function safely """
+
     try:
         result = fct(*args)
         return (result)
