ae9a4321e4e2f6510c77f0a0116d49eccb1613cf
luisdev20 <lescobedo.im@gmail.com>
2021-06-03 02:10:07 +0000

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+
+    with open(filename, encoding='utf-8') as a_file:
+        print (a_file.read(), end='')
