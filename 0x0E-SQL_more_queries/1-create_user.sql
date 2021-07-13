26b3056a4a17112cfd91d3d702c7d810e08c19c8
luisdev20 <lescobedo.im@gmail.com>
2021-07-13 22:19:44 +0000

    flush added

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- Script that creates the MySQL server user user_0d_1.
 -- If the user user_0d_1 already exists, your script should not fail.
-DROP USER 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
