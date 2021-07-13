c18b4672a5ebad020f533d6603cb98de01746b2c
luisdev20 <lescobedo.im@gmail.com>
2021-07-13 22:12:47 +0000

    create and drop users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- on your server (in localhost).
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Script that creates the MySQL server user user_0d_1.
+-- If the user user_0d_1 already exists, your script should not fail.
+DROP USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
