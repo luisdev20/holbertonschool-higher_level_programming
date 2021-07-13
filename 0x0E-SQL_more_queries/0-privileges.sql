725b5c98ee2c2b98c2a8a7821f81bc98aaec941f
luisdev20 <lescobedo.im@gmail.com>
2021-07-13 21:33:33 +0000

    GRANT functions

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- on your server (in localhost).
+GRANT ALL PRIVILEGES
+ON *.*
+TO 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
