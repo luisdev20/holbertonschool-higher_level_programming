50bbd67b5375cceda5336708f9252b67a2e93f50
luisdev20 <lescobedo.im@gmail.com>
2021-07-14 13:58:43 +0000

    Inner & outer multiple join

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Script that uses the hbtn_0d_tvshows database to lists all genres of Dexter.
+-- The tv_shows table contains only one record where title = Dexter (id c/b dif)
+SELECT g.`name`
+FROM tv_genres AS g
+     INNER JOIN tv_show_genres as sg
+          ON g.`id` = sg.`genre_id`
+     INNER JOIN tv_shows as s
+          ON sg.`show_id` = s.`id`
+WHERE s.`title` = 'Dexter'
+ORDER BY g.`name` ASC;
