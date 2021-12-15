379bffd0792959fe125d9d627b085d77720e56f3
luisdev20 <lescobedo.im@gmail.com>
2021-12-15 12:36:51 -0500

    not reduced

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  let counter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
 };
