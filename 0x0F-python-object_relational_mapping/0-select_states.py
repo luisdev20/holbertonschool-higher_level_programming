c03769091e9e0d422af97c8098a533ed591a0984
luisdev20 <lescobedo.im@gmail.com>
2021-12-18 02:20:46 -0500

    pycodestyle solved line 8 more than 79 chars

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,11 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
-    conn.close()
+    con.close()
