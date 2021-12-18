5bf49b0205500cd6524457fe668c5643695e6684
luisdev20 <lescobedo.im@gmail.com>
2021-12-18 18:02:47 -0500

    Flag makes it easy

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    all_states = session.query(State).order_by(State.id)
+    flag = 0
+    for each in all_states:
+        if sys.argv[4] == each.name:
+            print(each.id)
+            flag += 1
+            break
+    if flag == 0:
+        print("Not found")
