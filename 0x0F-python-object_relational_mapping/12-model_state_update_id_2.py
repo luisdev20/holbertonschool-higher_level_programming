22b1ddf09e75d30fd9cfb58797e4551d12bd2efe
luisdev20 <lescobedo.im@gmail.com>
2021-12-18 19:29:59 -0500

    first update in mysqlalchemy

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_mexico = session.query(State).filter(State.id == '2').first()
+    new_mexico.name = "New Mexico"
+    session.commit()
