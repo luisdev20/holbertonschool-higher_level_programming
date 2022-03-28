72443e0eece004078f4a75e103301d89e6ab707e
luisdev20 <lescobedo.im@gmail.com>
2022-03-27 19:09:46 -0500

    update

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that create a function that increments and calls a function
 function addMeMaybe (number, theFunction) {
-    theFunction(number + 1);
-  }
-  exports.addMeMaybe = addMeMaybe;
+  theFunction(number + 1);
+}
+exports.addMeMaybe = addMeMaybe;
